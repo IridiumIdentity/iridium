ada6800c939f6bede2a9ba08238d0c2e8c6f1ba0
Josh Fischer <josh@joshfischer.io>
2023-02-03 04:42:31 -0600

    adding enhanced switch statement

--- iridium-client/src/main/java/software/iridium/api/base/client/ErrorHandler.java
+++ iridium-client/src/main/java/software/iridium/api/base/client/ErrorHandler.java
@@ -14,22 +14,16 @@ public class ErrorHandler {
   public void handleErrors(final HttpStatus httpStatus) {
 
     switch (httpStatus) {
-      case UNAUTHORIZED:
-        throw new NotAuthorizedException(httpStatus.getReasonPhrase());
-      case FORBIDDEN:
-        throw new AccessDeniedException(httpStatus.getReasonPhrase());
-      case NOT_FOUND:
-        throw new ResourceNotFoundException(httpStatus.getReasonPhrase());
-      case CONFLICT:
-        throw new DuplicateResourceException(httpStatus.getReasonPhrase());
-      case INTERNAL_SERVER_ERROR:
-        throw new ClientCallException(httpStatus.getReasonPhrase());
-      default:
-        throw new ClientCallException(
-            "received "
-                + httpStatus.value()
-                + " response with reason "
-                + httpStatus.getReasonPhrase());
+      case UNAUTHORIZED -> throw new NotAuthorizedException(httpStatus.getReasonPhrase());
+      case FORBIDDEN -> throw new AccessDeniedException(httpStatus.getReasonPhrase());
+      case NOT_FOUND -> throw new ResourceNotFoundException(httpStatus.getReasonPhrase());
+      case CONFLICT -> throw new DuplicateResourceException(httpStatus.getReasonPhrase());
+      case INTERNAL_SERVER_ERROR -> throw new ClientCallException(httpStatus.getReasonPhrase());
+      default -> throw new ClientCallException(
+          "received "
+              + httpStatus.value()
+              + " response with reason "
+              + httpStatus.getReasonPhrase());
     }
   }
 }
