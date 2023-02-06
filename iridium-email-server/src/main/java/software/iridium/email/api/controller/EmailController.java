c91561898add8077b72b4b2499b89e4f8a8ec4f3
Josh Fischer <josh@joshfischer.io>
2023-02-06 06:34:06 -0600

    adding initial cloud build file; modifying state length

--- iridium-email-server/src/main/java/software/iridium/email/api/controller/EmailController.java
+++ iridium-email-server/src/main/java/software/iridium/email/api/controller/EmailController.java
@@ -27,6 +27,7 @@ public class EmailController {
       consumes = EmailSendRequest.MEDIA_TYPE,
       produces = EmailSendResponse.MEDIA_TYPE)
   public ApiDataResponse<EmailSendResponse> send(@RequestBody final EmailSendRequest request) {
-    return new ApiDataResponse<>(emailService.send(request));
+    // return new ApiDataResponse<>(emailService.send(request));
+    return new ApiDataResponse<>(null);
   }
 }
