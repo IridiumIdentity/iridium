c25fb6420307c6265278bb678a707d69b4ba531d
Josh Fischer <josh@joshfischer.io>
2023-05-17 11:11:53 -0500

    improving angular client handshake

--- iridium-angular-client/projects/iridium-ui/src/environments/environment.ts
+++ iridium-angular-client/projects/iridium-ui/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   iridium: {
     domain: 'http://localhost:8381/',
     redirectUri: 'http://localhost:4200/dashboard',
-    clientId: 'xd4rtddkthdfh234r',
+    clientId: 'your-client-id',
     errorPath: '/error'
   }
 };
