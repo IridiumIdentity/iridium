36f5e357b08baccbabe37870765b5680f5b33f93
Josh Fischer <josh@joshfischer.io>
2023-05-21 15:27:12 -0500

    checkstyles

--- iridium-core-server/src/main/java/software/iridium/api/Iridium.java
+++ iridium-core-server/src/main/java/software/iridium/api/Iridium.java
@@ -19,7 +19,6 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
