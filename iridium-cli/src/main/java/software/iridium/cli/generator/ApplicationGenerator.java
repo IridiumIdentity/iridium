f5b71a55c5149cc3a197cc531ccb41416dc3cc01
Josh Fischer <josh@joshfischer.io>
2023-06-04 16:24:17 -0500

    adding seed identity; randomizing client id

--- iridium-cli/src/main/java/software/iridium/cli/generator/ApplicationGenerator.java
+++ iridium-cli/src/main/java/software/iridium/cli/generator/ApplicationGenerator.java
@@ -12,23 +12,30 @@
 package software.iridium.cli.generator;
 
 import jakarta.persistence.EntityManager;
+import java.security.NoSuchAlgorithmException;
+import software.iridium.cli.util.EncoderUtils;
 import software.iridium.entity.ApplicationEntity;
 import software.iridium.entity.ApplicationTypeEntity;
 import software.iridium.entity.TenantEntity;
 
 public class ApplicationGenerator extends AbstractGenerator {
 
+  public static final Integer CLIENT_ID_SEED_LENGTH = 16;
+
   public static ApplicationEntity generateIridiumApplication(
       final EntityManager entityManager,
       final TenantEntity iridiumTenant,
-      final ApplicationTypeEntity applicationType) {
+      final ApplicationTypeEntity applicationType)
+      throws NoSuchAlgorithmException {
+    final var encoderUtils = new EncoderUtils();
+
     beginTransaction(entityManager);
     final var iridiumManagementApp = new ApplicationEntity();
     iridiumManagementApp.setHomePageUrl("http://localhost:4200");
     iridiumManagementApp.setName("iridium management app");
     iridiumManagementApp.setRedirectUri("http://localhost:4200/dashboard");
     iridiumManagementApp.setTenantId(iridiumTenant.getId());
-    iridiumManagementApp.setClientId("xd4rtddkthdfh234r");
+    iridiumManagementApp.setClientId(encoderUtils.cryptoSecureToHex(CLIENT_ID_SEED_LENGTH));
     iridiumManagementApp.setApplicationType(applicationType);
     entityManager.persist(iridiumManagementApp);
     flushAndCommitTransaction(entityManager);
