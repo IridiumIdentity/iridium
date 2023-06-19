7b50ff52ac83cd6dc92f7ad7bcc5135ed317480f
Tim Grein <tim@4greins.de>
2023-06-19 22:05:17 +0200

    Add DateUtilsTest (#75)
    * Add DateUtilsTest
    
    Add `DateUtilsTest` for the `DateUtils` class inside `iridium-server-base`.
    
    * Add mockito-inline and statically mock the Calendar.
    
    * Add mockito-inline
    
    * Rename "calendarMock" to "calendarClassMock".
    
    * Replace core with inline

---
+++ iridium-server-base/src/test/java/software/iridium/api/DateUtilsTest.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package software.iridium.api;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Calendar;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.iridium.api.util.DateUtils;
+
+@ExtendWith(MockitoExtension.class)
+public class DateUtilsTest {
+
+  @Mock private Calendar calendarInstanceMock;
+
+  @Test
+  public void addHoursToCurrentTime_AllGood_FiveHoursAdded() {
+    try (MockedStatic<Calendar> calendarClassMock = Mockito.mockStatic(Calendar.class)) {
+      when(calendarInstanceMock.getTime()).thenReturn(new Date());
+      calendarClassMock.when(Calendar::getInstance).thenReturn(calendarInstanceMock);
+
+      final var hours = 5;
+      Date dateAfterAddingFiveHoursToCurrentTime = DateUtils.addHoursToCurrentTime(hours);
+
+      verify(calendarInstanceMock).add(Calendar.HOUR_OF_DAY, hours);
+      assertNotNull(dateAfterAddingFiveHoursToCurrentTime);
+    }
+  }
+
+  @Test
+  public void addMinutesToCurrentTime_AllGood_FiveMinutesAdded() {
+    try (MockedStatic<Calendar> calendarClassMock = Mockito.mockStatic(Calendar.class)) {
+      when(calendarInstanceMock.getTime()).thenReturn(new Date());
+      calendarClassMock.when(Calendar::getInstance).thenReturn(calendarInstanceMock);
+
+      final var minutes = 5;
+      Date dateAfterAddingFiveMinutesToCurrentTime = DateUtils.addMinutesToCurrentTime(minutes);
+
+      verify(calendarInstanceMock).add(Calendar.MINUTE, minutes);
+      assertNotNull(dateAfterAddingFiveMinutesToCurrentTime);
+    }
+  }
+}
