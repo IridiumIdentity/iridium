0b0f3c842e073d88ed3d751c041f3526ea9ff3fd
Josh Fischer <josh@joshfischer.io>
2023-06-21 07:20:55 -0500

    revert environment

--- iridium-angular-client/projects/iridium-ui/src/environments/environment.ts
+++ iridium-angular-client/projects/iridium-ui/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     domain: 'http://localhost:8381/',
     redirectUri: 'http://localhost:4200/callback',
     successfulAuthDestination: '/dashboard',
-    clientId: 'd9ffd4140f28e56f732c5a6975b7cdab',
+    clientId: 'YOUR_CLIENT_ID',
     errorPath: '/error'
   }
 };
