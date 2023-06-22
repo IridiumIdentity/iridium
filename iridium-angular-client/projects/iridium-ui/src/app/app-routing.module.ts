dee02eb03564c5a3ee99786cd554676662e1561e
Josh Fischer <josh@joshfischer.io>
2023-06-22 15:58:28 -0500

    Joshfischer/simplify UI (#79)
    * in progress of handling errors
    
    * simplify client
    
    * clean up dead code
    
    * clean up logging
    
    * clean up comments
    
    * label change, remove dead files
    
    * revert environment
    
    * add tests

--- iridium-angular-client/projects/iridium-ui/src/app/app-routing.module.ts
+++ iridium-angular-client/projects/iridium-ui/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgxIridiumCallbackComponent } from 'ngx-iridium-client';
+import { authGuard } from './guard/auth.guard';
 
 
 
 const routes: Routes = [
   { path: '', component: RegisterComponent},
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
   { path: 'callback', component: NgxIridiumCallbackComponent },
 ];
 
