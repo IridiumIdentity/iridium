070574e43ff36063b86238419a01ed52d15458ff
Tim Grein <tim@4greins.de>
2023-06-29 04:29:00 +0200

    Add custom error page for a NotAuthorizedException (#86)
    * Add AttributeValidatorTest
    
    * Improve some test names
    
    * Autoformat
    
    * Autoformat
    
    * Add .eslintrc.json
    
    * Update angular.json with lint targets
    
    * Update nightly builds to lint angular clients before building them
    
    * Update package files
    
    * Update package files
    
    * Readd angular-devkit/build-angular
    
    * Change "lint-all" to "lint"
    
    * Return view, when running into an NotAuthorizedException.
    
    * Add ApiExceptionHandlerTest
    
    * Use common.css file in authorize.html
    
    * Update error.html page to show the message of the thrown exception
    
    * Remove authorize
    
    * Return JSON, if the accept header specifies JSON as expected media type

--- iridium-core-server/src/main/java/software/iridium/api/ApiExceptionHandler.java
+++ iridium-core-server/src/main/java/software/iridium/api/ApiExceptionHandler.java
@@ -11,11 +11,15 @@
  */
 package software.iridium.api;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 import software.iridium.api.base.domain.ApiResponse;
 import software.iridium.api.base.error.*;
 
@@ -30,13 +34,28 @@ public class ApiExceptionHandler {
 
   @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
   @ExceptionHandler(NotAuthorizedException.class)
-  public @ResponseBody ApiResponse handleNotAuthorizedException(final Exception e) {
-    return new ApiResponse(HttpStatus.UNAUTHORIZED.toString(), e.getMessage());
+  public @ResponseBody Object handleNotAuthorizedException(
+      final NotAuthorizedException e, final WebRequest request) {
+    final var acceptHeader = request.getHeader(HttpHeaders.ACCEPT);
+    final var shouldReturnJson =
+        acceptHeader != null && acceptHeader.contains(MediaType.APPLICATION_JSON_VALUE);
+
+    if (shouldReturnJson) {
+      return new ApiResponse(HttpStatus.UNAUTHORIZED.toString(), e.getMessage());
+    }
+
+    final var modelAndView = new ModelAndView();
+
+    modelAndView.addObject("statusCode", e.getCode());
+    modelAndView.addObject("errorMessage", e.getMessage());
+    modelAndView.setViewName("error");
+
+    return modelAndView;
   }
 
   @ResponseStatus(value = HttpStatus.CONFLICT)
   @ExceptionHandler(DuplicateResourceException.class)
-  public @ResponseBody ApiResponse handeDuplicateResourceException(final Exception e) {
+  public @ResponseBody ApiResponse handleDuplicateResourceException(final Exception e) {
     return new ApiResponse(HttpStatus.CONFLICT.toString(), e.getMessage());
   }
 
