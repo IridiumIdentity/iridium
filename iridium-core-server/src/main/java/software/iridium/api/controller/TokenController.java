fe9414b4db1560c95836fba60be551a746edf40d
Josh Fischer <josh@joshfischer.io>
2023-09-15 20:59:54 -0500

    add logout url

---
+++ iridium-core-server/src/main/java/software/iridium/api/controller/TokenController.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package software.iridium.api.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import software.iridium.api.service.TokenManager;
+
+@CrossOrigin
+@RestController
+public class TokenController {
+
+  @Autowired private TokenManager tokenManager;
+
+  @DeleteMapping(value = "applications/{clientId}/tokens")
+  public void logout(
+      @PathVariable(value = "clientId") final String clientId,
+      HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse) {
+    tokenManager.logout(clientId, httpServletRequest);
+    httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+  }
+}
