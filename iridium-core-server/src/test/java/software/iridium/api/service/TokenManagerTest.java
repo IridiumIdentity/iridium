9d8de2151cd379a130f953d5bfa4c40d68c3907a
Josh Fischer <josh@joshfischer.io>
2023-09-16 08:08:34 -0500

    add logout url (#181)
    * add logout url
    
    * add doc

--- iridium-core-server/src/test/java/software/iridium/api/service/TokenManagerTest.java
+++ iridium-core-server/src/test/java/software/iridium/api/service/TokenManagerTest.java
@@ -14,21 +14,34 @@ package software.iridium.api.service;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
 import org.apache.commons.lang3.time.DateUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import software.iridium.api.base.error.NotAuthorizedException;
+import software.iridium.api.repository.AccessTokenEntityRepository;
+import software.iridium.api.repository.ApplicationEntityRepository;
 import software.iridium.api.repository.AuthenticationEntityRepository;
+import software.iridium.api.repository.IdentityEntityRepository;
+import software.iridium.api.util.AttributeValidator;
+import software.iridium.api.util.ServletTokenExtractor;
+import software.iridium.entity.AccessTokenEntity;
+import software.iridium.entity.ApplicationEntity;
 import software.iridium.entity.AuthenticationEntity;
 import software.iridium.entity.IdentityEntity;
 
@@ -37,7 +50,26 @@ class TokenManagerTest {
 
   @Mock private AuthenticationEntityRepository mockAuthenticationEntityRepository;
   @Mock private AuthenticationGenerator mockAuthenticationGenerator;
+  @Mock private HttpServletRequest mockServletRequest;
+  @Mock private IdentityEntityRepository mockIdentityRepository;
+  @Mock private AttributeValidator mockValidator;
+  @Mock private AccessTokenEntityRepository mockAccessTokenRepository;
+  @Mock private ServletTokenExtractor mockTokenExtractor;
+  @Mock private ApplicationEntityRepository mockApplicationRepository;
   @InjectMocks private TokenManager subject;
+
+  @AfterEach
+  public void ensureNoUnexpectedMockInteractions() {
+    Mockito.verifyNoMoreInteractions(
+        mockAuthenticationEntityRepository,
+        mockAuthenticationGenerator,
+        mockServletRequest,
+        mockIdentityRepository,
+        mockValidator,
+        mockAccessTokenRepository,
+        mockTokenExtractor,
+        mockApplicationRepository);
+  }
 
   @Test
   public void getOrGenerateToken_AuthenticationFound_NewestTokenExpired_BehavesAsExpected() {
@@ -133,5 +165,133 @@ class TokenManagerTest {
 
     assertThat(pair.getLeft(), is(equalTo(authToken)));
     assertThat(pair.getRight(), is(equalTo(refreshToken)));
+  }
+
+  @Test
+  public void logout_AllGood_BehavesAsExpected() {
+    final var clientId = "the client id";
+    final var tokenValue = "the tokenValue";
+    final var identityId = "the identity id";
+    final var tenantId = "the tenant id";
+    final var accessToken = new AccessTokenEntity();
+    accessToken.setIdentityId(identityId);
+    final var identity = new IdentityEntity();
+    identity.setParentTenantId(tenantId);
+    final var application = new ApplicationEntity();
+    application.setTenantId(tenantId);
+
+    when(mockValidator.isNotBlank(same(clientId))).thenReturn(true);
+    when(mockTokenExtractor.extractBearerToken(same(mockServletRequest))).thenReturn(tokenValue);
+    when(mockAccessTokenRepository.findFirstByAccessTokenAndExpirationAfter(
+            same(tokenValue), any(Date.class)))
+        .thenReturn(Optional.of(accessToken));
+    when(mockIdentityRepository.findById(same(identityId))).thenReturn(Optional.of(identity));
+    when(mockApplicationRepository.findByClientId(same(clientId)))
+        .thenReturn(Optional.of(application));
+
+    subject.logout(clientId, mockServletRequest);
+
+    verify(mockValidator).isNotBlank(same(clientId));
+    verify(mockTokenExtractor).extractBearerToken(same(mockServletRequest));
+    verify(mockAccessTokenRepository)
+        .findFirstByAccessTokenAndExpirationAfter(same(tokenValue), any(Date.class));
+    verify(mockIdentityRepository).findById(same(identityId));
+    verify(mockApplicationRepository).findByClientId(same(clientId));
+    verify(mockAccessTokenRepository).delete(accessToken);
+  }
+
+  @Test
+  public void logout_AccessTokenNotFound_ReturnsNull() {
+    final var clientId = "the client id";
+    final var tokenValue = "the tokenValue";
+    final var identityId = "the identity id";
+    final var tenantId = "the tenant id";
+    final var accessToken = new AccessTokenEntity();
+    accessToken.setIdentityId(identityId);
+    final var identity = new IdentityEntity();
+    identity.setParentTenantId(tenantId);
+    final var application = new ApplicationEntity();
+    application.setTenantId(tenantId);
+
+    when(mockValidator.isNotBlank(same(clientId))).thenReturn(true);
+    when(mockTokenExtractor.extractBearerToken(same(mockServletRequest))).thenReturn(tokenValue);
+    when(mockAccessTokenRepository.findFirstByAccessTokenAndExpirationAfter(
+            same(tokenValue), any(Date.class)))
+        .thenReturn(Optional.empty());
+
+    subject.logout(clientId, mockServletRequest);
+
+    verify(mockValidator).isNotBlank(same(clientId));
+    verify(mockTokenExtractor).extractBearerToken(same(mockServletRequest));
+    verify(mockAccessTokenRepository)
+        .findFirstByAccessTokenAndExpirationAfter(same(tokenValue), any(Date.class));
+  }
+
+  @Test
+  public void logout_IdentityNotFound_ExceptionThrown() {
+    final var clientId = "the client id";
+    final var tokenValue = "the tokenValue";
+    final var identityId = "the identity id";
+    final var tenantId = "the tenant id";
+    final var accessToken = new AccessTokenEntity();
+    accessToken.setIdentityId(identityId);
+    final var identity = new IdentityEntity();
+    identity.setParentTenantId(tenantId);
+    final var application = new ApplicationEntity();
+    application.setTenantId(tenantId);
+
+    when(mockValidator.isNotBlank(same(clientId))).thenReturn(true);
+    when(mockTokenExtractor.extractBearerToken(same(mockServletRequest))).thenReturn(tokenValue);
+    when(mockAccessTokenRepository.findFirstByAccessTokenAndExpirationAfter(
+            same(tokenValue), any(Date.class)))
+        .thenReturn(Optional.of(accessToken));
+    when(mockIdentityRepository.findById(same(identityId))).thenReturn(Optional.empty());
+
+    final var exception =
+        assertThrows(
+            NotAuthorizedException.class, () -> subject.logout(clientId, mockServletRequest));
+
+    assertThat(exception.getMessage(), is(equalTo("Not Authorized")));
+
+    verify(mockValidator).isNotBlank(same(clientId));
+    verify(mockTokenExtractor).extractBearerToken(same(mockServletRequest));
+    verify(mockAccessTokenRepository)
+        .findFirstByAccessTokenAndExpirationAfter(same(tokenValue), any(Date.class));
+    verify(mockIdentityRepository).findById(same(identityId));
+  }
+
+  @Test
+  public void logout_ApplicationNotFound_ExceptionThrown() {
+    final var clientId = "the client id";
+    final var tokenValue = "the tokenValue";
+    final var identityId = "the identity id";
+    final var tenantId = "the tenant id";
+    final var accessToken = new AccessTokenEntity();
+    accessToken.setIdentityId(identityId);
+    final var identity = new IdentityEntity();
+    identity.setParentTenantId(tenantId);
+    final var application = new ApplicationEntity();
+    application.setTenantId(tenantId);
+
+    when(mockValidator.isNotBlank(same(clientId))).thenReturn(true);
+    when(mockTokenExtractor.extractBearerToken(same(mockServletRequest))).thenReturn(tokenValue);
+    when(mockAccessTokenRepository.findFirstByAccessTokenAndExpirationAfter(
+            same(tokenValue), any(Date.class)))
+        .thenReturn(Optional.of(accessToken));
+    when(mockIdentityRepository.findById(same(identityId))).thenReturn(Optional.of(identity));
+    when(mockApplicationRepository.findByClientId(same(clientId))).thenReturn(Optional.empty());
+
+    final var exception =
+        assertThrows(
+            NotAuthorizedException.class, () -> subject.logout(clientId, mockServletRequest));
+
+    assertThat(exception.getMessage(), is(equalTo("Not Authorized")));
+
+    verify(mockValidator).isNotBlank(same(clientId));
+    verify(mockTokenExtractor).extractBearerToken(same(mockServletRequest));
+    verify(mockAccessTokenRepository)
+        .findFirstByAccessTokenAndExpirationAfter(same(tokenValue), any(Date.class));
+    verify(mockIdentityRepository).findById(same(identityId));
+    verify(mockApplicationRepository).findByClientId(same(clientId));
   }
 }
