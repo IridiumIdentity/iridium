1f3210bcf50835ebb9cb1b4e787ce28338e99043
zhajikun <zhajikun@hotmail.com>
2023-09-17 22:51:25 -0500

    #174 add liquibase h2 for init tables in DB

---
+++ iridium-core-server/src/main/resources/db/changelog/DBchanges/02-insert-data-publisher.sql
@@ -0,0 +1,8 @@
+
+--liquibase formatted sql
+--changeset jakeChrysler:2
+
+INSERT INTO publisher (id, name) VALUES (1, 'publisher 1');
+INSERT INTO publisher (id, name) VALUES (2, 'publisher 2');
+INSERT INTO publisher (id, name) VALUES (3, 'publisher 3');
+INSERT INTO publisher (id, name) VALUES (4, 'publisher 4');
