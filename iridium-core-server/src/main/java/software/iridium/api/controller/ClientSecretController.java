13106bbe876ed090142459617d620e10b648043f
Josh Fischer <josh@joshfischer.io>
2023-09-26 18:56:18 -0500

    client secret (#200)

--- iridium-core-server/src/main/java/software/iridium/api/controller/ClientSecretController.java
+++ iridium-core-server/src/main/java/software/iridium/api/controller/ClientSecretController.java
@@ -15,7 +15,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import software.iridium.api.authentication.domain.ClientSecretCreateResponse;
-import software.iridium.api.base.domain.ApiDataResponse;
 import software.iridium.api.base.domain.ApiResponse;
 import software.iridium.api.service.ClientSecretService;
 
@@ -26,15 +25,14 @@ public class ClientSecretController {
   @Autowired private ClientSecretService clientSecretService;
 
   @PostMapping(
-      value = "/tenant/{tenant-id}/applications/{application-id}/client-secrets",
+      value = "/tenants/{tenant-id}/applications/{application-id}/client-secrets",
       produces = ClientSecretCreateResponse.MEDIA_TYPE)
-  public ApiDataResponse<ClientSecretCreateResponse> create(
+  public ClientSecretCreateResponse create(
       HttpServletRequest servletRequest,
       @PathVariable(name = "tenant-id") final String tenantId,
       @PathVariable(name = "application-id") final String applicationId) {
 
-    return new ApiDataResponse<>(
-        clientSecretService.create(servletRequest, tenantId, applicationId));
+    return clientSecretService.create(servletRequest, tenantId, applicationId);
   }
 
   @DeleteMapping(value = "applications/{application-id}/client-secrets/{client-secret-id}")
