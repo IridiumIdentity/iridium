950482a6edddff0f2cad5b6f97be1f4ffcd36be2
Gianni Crivello <gianniemperado@gmail.com>
2023-09-27 20:14:20 -0400

    PR test
    just a test for discord webhook

--- iridium-cli/src/main/java/software/iridium/cli/command/InitCommand.java
+++ iridium-cli/src/main/java/software/iridium/cli/command/InitCommand.java
@@ -8,6 +8,7 @@
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
+ * Iridium CLI
  */
 package software.iridium.cli.command;
 
